52bd196d64217be8b7ca56ae50a2cfea4ce41afa
friddler <yhma22.nilsfr@folkuniversitetet.nu>
2023-05-10 12:48:37 +0200

    Initial Commit

---
+++ CalmCompass/CalmCompassApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalmCompassApp.swift
+//  CalmCompass
+//
+//  Created by Frida Nilsson on 2023-05-10.
+//
+
+import SwiftUI
+
+@main
+struct CalmCompassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
